ee4ca55ada9e4cdda3d1fc5f5a01f8a44c956796
Aymeric SONOLET - DEN/DANS/DM2S/STMF/LGLS - 2019/04/26 <aymeric.sonolet@cea.fr>
2022-08-29 14:28:12 +0200

    ajout plus de statistiques

--- src/statistics.py
+++ src/statistics.py
@@ -14,39 +14,92 @@
 ##############################################################################
 
 import numpy as np
-from copy import deepcopy
-from src.problem_definition import PhysicalProperties, NumericalProperties
 from src.main import StateProblem
 
 
 class Statistics:
     def __init__(self):
-        self.t = None
-        self.E = None
-        self.Tl = None
-        self.Tv = None
+        self.time_stats = {
+            "t": TimeStatArray(),
+            "E": TimeStatArray(),
+            "El": TimeStatArray(),
+            "Ev": TimeStatArray(),
+            "El_pure": TimeStatArray(),
+            "Ev_pure": TimeStatArray(),
+            "T": TimeStatArray(),
+            "Tl": TimeStatArray(),
+            "Tv": TimeStatArray(),
+            "Tl_pure": TimeStatArray(),
+            "Tv_pure": TimeStatArray(),
+        }
         self.n = 0
         self.step = 0
 
+    def copy(self, other):
+        self.n = other.n
+        self.step = other.step
+        for key in self.time_stats.keys():
+            if key in other.time_stats.keys():
+                self.time_stats[key].data = other.time_stats[key].data.copy()
+            else:
+                print(key, "was not saved in", other, "object")
+                self.time_stats[key].alloc(self.n + 1)
+
     def collect(self, pb: StateProblem):
-        self.t[self.step] = pb.time
-        self.E[self.step] = pb.energy
-        self.Tl[self.step] = np.sum(pb.T * pb.I) / np.sum(pb.I)
-        self.Tv[self.step] = np.sum(pb.T * (1.0 - pb.I)) / np.sum(1.0 - pb.I)
+        self.time_stats["t"][self.step] = pb.time
+        self.time_stats["E"][self.step] = pb.energy
+        self.time_stats["El"][self.step] = np.sum(
+            pb.T * pb.rho_cp.a(pb.I) * pb.I
+        ) / np.sum(pb.I)
+        self.time_stats["Ev"][self.step] = np.sum(
+            pb.T * pb.rho_cp.a(pb.I) * (1.0 - pb.I)
+        ) / np.sum(1.0 - pb.I)
+        self.time_stats["El_pure"][self.step] = np.sum(
+            np.where(pb.I == 1.0, pb.T * pb.rho_cp.a(pb.I), 0.0)
+        ) / np.sum(np.where(pb.I == 1.0, 1.0, 0.0))
+        self.time_stats["Ev_pure"][self.step] = np.sum(
+            np.where(pb.I == 0.0, pb.T * pb.rho_cp.a(pb.I), 0.0)
+        ) / np.sum(np.where(pb.I == 0.0, 1.0, 0.0))
+        self.time_stats["T"][self.step] = np.sum(pb.T) / pb.T.size
+        self.time_stats["Tl"][self.step] = np.sum(pb.T * pb.I) / np.sum(pb.I)
+        self.time_stats["Tv"][self.step] = np.sum(pb.T * (1.0 - pb.I)) / np.sum(
+            1.0 - pb.I
+        )
+        self.time_stats["Tl_pure"][self.step] = np.sum(
+            np.where(pb.I == 1.0, pb.T, 0.0)
+        ) / np.sum(np.where(pb.I == 1.0, 1.0, 0.0))
+        self.time_stats["Tv_pure"][self.step] = np.sum(
+            np.where(pb.I == 0.0, pb.T, 0.0)
+        ) / np.sum(np.where(pb.I == 0.0, 1.0, 0.0))
         self.step += 1
 
     def extend(self, longer_n):
-        assert longer_n > self.n
         self.n = longer_n
-        if self.E is None:
+        if self.E.size == 0:
             self.step = 0
-            self.t = np.zeros((longer_n + 1,))
-            self.E = np.zeros((longer_n + 1,))
-            self.Tl = np.zeros((longer_n + 1,))
-            self.Tv = np.zeros((longer_n + 1,))
+            for stat in self.time_stats.values():
+                stat.alloc(longer_n + 1)
         else:
             self.step = self.E.size - 1
-            self.E = np.r_[self.E, np.zeros((self.n,))]
-            self.t = np.r_[self.t, np.zeros((self.n,))]
-            self.Tl = np.r_[self.Tl, np.zeros((self.n,))]
-            self.Tv = np.r_[self.Tv, np.zeros((self.n,))]
+            for stat in self.time_stats.values():
+                stat.extend(longer_n)
+
+    def __getattr__(self, item):
+        return self.time_stats[item].data
+
+
+class TimeStatArray:
+    def __init__(self):
+        self.data = np.array([])  # type: np.ndarray
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def alloc(self, n):
+        self.data = np.zeros((n,))
+
+    def extend(self, n):
+        self.data = np.r_[self.data, np.zeros((n,))]
